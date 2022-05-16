8d5dd51c053719335fb8e0e9b3648544a3260b08
Armen Gulakhszyan <agulakhszyan@tradingview.com>
2022-05-17 00:35:30 +0300

    add validation

--- jwt-auth-server/src/authorization/dto/sign-up.dto.ts
+++ jwt-auth-server/src/authorization/dto/sign-up.dto.ts
@@ -1,5 +1,9 @@
+import { IsEmail, Length } from 'class-validator';
+
 export class SignUpDto {
+	@IsEmail()
 	public readonly email: string;
 
+	@Length(6, 32)
 	public readonly password: string;
 }
--- jwt-auth-server/src/main.ts
+++ jwt-auth-server/src/main.ts
@@ -3,16 +3,19 @@ import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 config();
 const port = process.env.PORT || 3333;
 
 async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule);
+	
+	app.enableCors();
 
 	app.use(cookieParser());
 
-	app.enableCors();
+	app.useGlobalPipes(new ValidationPipe());
 
 	await app.listen(port, () => {
 		console.log(`Server started on port ${port}`);
