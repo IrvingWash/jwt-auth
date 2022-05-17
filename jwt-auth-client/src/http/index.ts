17eb4beb36f1272747347c4d9ba1629f387abcd7
Armen Gulakhszyan <agulakhszyan@tradingview.com>
2022-05-17 21:55:31 +0300

    final fix

--- jwt-auth-client/src/http/index.ts
+++ jwt-auth-client/src/http/index.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+import { AuthResponse } from '../models/response/auth-response';
+
 export const baseUrl = 'http://localhost:3333';
 
 const api = axios.create({
@@ -16,5 +18,35 @@ api.interceptors.request.use((config) => {
 
 	return config;
 });
+
+api.interceptors.response.use(
+	(config) => {
+		return config;
+	},
+	async (error) => {
+		if (/* error.response.status === 401 && */ error.config && !error.config._isRetry) {
+			const originalRequest = error.config;
+	
+			originalRequest._isRetry = true;
+	
+			try {
+				const response = await axios.get<AuthResponse>(
+					`${baseUrl}/auth/refresh`,
+					{ withCredentials: true }
+				);
+	
+				localStorage.setItem('token', response.data.accessToken);
+	
+				return api.request(originalRequest);
+	
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			} catch (error: any) {
+				throw new Error(error);
+			}
+		}
+
+		throw new Error(error);
+	}
+);
 
 export default api;
